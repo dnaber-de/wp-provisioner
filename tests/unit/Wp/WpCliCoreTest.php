d6c659f6c250b8a110c92365130e640463f5ebce
David Naber <kontakt@dnaber.de>
2016-05-29 11:09:40 +0200

    Init unit test setup

---
+++ tests/unit/Wp/WpCliCoreTest.php
@@ -0,0 +1,15 @@
+<?php # -*- coding: utf-8 -*-
+
+namespace WpProvision\Wp;
+
+use
+	Mockery,
+	MonkeryTestCase;
+
+class WpCliCoreTest extends MonkeryTestCase\MockeryTestCase {
+
+	public function testIsInstalled() {
+
+		$this->markTestIncomplete();
+	}
+}
