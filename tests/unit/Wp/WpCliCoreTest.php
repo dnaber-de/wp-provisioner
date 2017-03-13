d3ce137d9e02c622fa7f68bf9552a8d2a41d8407
David Naber <kontakt@dnaber.de>
2017-03-13 16:15:38 +0100

    Starting tests for WpCliCore

--- tests/unit/Wp/WpCliCoreTest.php
+++ tests/unit/Wp/WpCliCoreTest.php
@@ -3,13 +3,61 @@
 namespace WpProvision\Wp;
 
 use
+	WpProvision\Command,
+	WpProvision\Utils,
 	Mockery,
 	MonkeryTestCase;
 
 class WpCliCoreTest extends MonkeryTestCase\MockeryTestCase {
 
+	/**
+	 * @see WpCliCore::isInstalled
+	 */
 	public function testIsInstalled() {
 
-		$this->markTestIncomplete();
+		$wp_cli_mock = Mockery::mock( Command\WpCliCommand::class );
+		$wp_cli_mock->shouldReceive( 'run' )
+			->with( [ 'core', 'is-installed' ] );
+
+		$testee = new WpCliCore(
+			$wp_cli_mock,
+			Mockery::mock( Utils\PasswordGenerator::class )
+		);
+
+		$this->assertTrue( $testee->isInstalled() );
+	}
+
+	/**
+	 * @see WpCliCore::isInstalled
+	 */
+	public function testIsInstalledNetwork() {
+
+		$wp_cli_mock = Mockery::mock( Command\WpCliCommand::class );
+		$wp_cli_mock->shouldReceive( 'run' )
+			->with( [ 'core', 'is-installed', '--network' ] );
+
+		$testee = new WpCliCore(
+			$wp_cli_mock,
+			Mockery::mock( Utils\PasswordGenerator::class )
+		);
+
+		$this->assertTrue( $testee->isInstalled( TRUE ) );
+	}
+
+	/**
+	 * @see WpCliCore::isInstalled
+	 */
+	public function testIsInstalledException() {
+
+		$wp_cli_mock = Mockery::mock( Command\WpCliCommand::class );
+		$wp_cli_mock->shouldReceive( 'run' )
+			->with( [ 'core', 'is-installed' ] );
+
+		$testee = new WpCliCore(
+			$wp_cli_mock,
+			Mockery::mock( Utils\PasswordGenerator::class )
+		);
+
+		#$this->assertTrue( $testee->isInstalled( TRUE ) );
 	}
 }
