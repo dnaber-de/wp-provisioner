cf3b883a867440c690c30afc43721f9ebbb96006
David Naber <kontakt@dnaber.de>
2017-03-24 09:53:28 +0100

    Pass CWD to WpCliCommandFactory

--- src/Factory/WpCliCommandFactory.php
+++ src/Factory/WpCliCommandFactory.php
@@ -49,42 +49,45 @@ class WpCliCommandFactory {
 
 	/**
 	 * @param string $wp_cli
+	 * @param string $cwd
 	 *
 	 * @return GenericCommand
 	 */
-	public function getWpCliCommand( $wp_cli = '' ) {
+	public function getWpCliCommand( $wp_cli = '', $cwd = '' ) {
 
 		$wp_cli = (string) $wp_cli;
 		$wp_cli or $wp_cli = 'wp';
+		$cwd or $cwd = getcwd();
 
 		if ( $this->shell->commandExists( $wp_cli ) ) {
-			return $this->buildCommand( [ $wp_cli ] );
+			return $this->buildCommand( [ $wp_cli ], $cwd );
 		}
 
 		// if the command points to an executable file
 		if ( $this->shell->isReadable( $wp_cli ) && $this->shell->isExecutable( $wp_cli ) ) {
-			return $this->buildCommand( [ $wp_cli ] );
+			return $this->buildCommand( [ $wp_cli ], $cwd );
 		}
 
 		$php_bin = $this->php_finder->find();
 		// if it points to a file, assume it's a php script
 		if ( $this->shell->isReadable( $wp_cli ) && $php_bin ) {
-			return $this->buildCommand( [ $php_bin, $wp_cli ] );
+			return $this->buildCommand( [ $php_bin, $wp_cli ], $cwd );
 		}
 
 		// maybe fallback to 'wp'
 		$wp_bin = $this->exec_finder->find( 'wp' );
 		if ( $wp_bin ) {
-			return $this->buildCommand( [ $wp_bin ] );
+			return $this->buildCommand( [ $wp_bin ], $cwd );
 		}
 
 		throw new WpCliNotFound( "Could not found command {$wp_cli}" );
 	}
 
-	private function buildCommand( array $base ) {
+	private function buildCommand( array $base, $cwd ) {
 
 		$provider = new SymfonyProcessBuilderAdapter();
 		$provider->setPrefix( $base );
+		$provider->setWorkingDirectory( $cwd );
 
 		return new GenericCommand( $provider );
 	}
